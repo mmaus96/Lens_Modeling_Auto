9f818e749f190015b27b4c7f58baef706e2a0374
mmaus96 <61140454+mmaus96@users.noreply.github.com>
2021-07-26 21:36:59 -0600

    Update main_modeling_script.py

--- main_modeling_script.py
+++ main_modeling_script.py
@@ -55,11 +55,10 @@ if not exists(results_path): #creates results folder if it doesn't already exist
     os.mkdir(results_path)
 
 #Folder names for data, psf, noise map, original image [TO DO BY USER]
-im_path = data_path + '/data'
-# im_path = data_path + '/simulations'
-psf_path = data_path + '/psf'
-noise_path = data_path + '/psf'
-noise_type = 'EXPTIME' # 'NOISE_MAP' or 'EXPTIME'
+im_path = data_path + '/' #add name of folder with image data
+psf_path = data_path + '/' #add name of folder with psf data
+noise_path = data_path + '/' #add name of folder with rms data, OR folder with FITS files that contain exposure times in header files (if using 'EXPTIME' for noise_type) 
+noise_type = '' # 'NOISE_MAP' or 'EXPTIME'
 band_list = ['g','r','i'] #list of bands
 obj_name_location = 0 # index corresponding to which string of numbers in filenames are the ID 
 
@@ -98,12 +97,12 @@ select_objects =  None #['03310601','06653211','06788344','14083401',
 #                     '19990514']  #List of strings with object IDs, or None
 
 # Additional info for images [TO DO BY USER]
-deltaPix = 0.27 #pixel scale of the images in arcsec/pixel
-zeroPt = 30 #not used anywhere
+deltaPix =  #pixel scale of the images in arcsec/pixel
+zeroPt = 30 #not used anywhere - can put anything
 psf_upsample_factor = 1 #If psf is upsampled
-ra_dec = 'csv' # 'csv', 'header', or 'None'. Where to find ra and dec values if desired for naming. Otherwise will have 'N/A' in RA and DEC columns of results
-ra_dec_loc = '.csv' #path to csv file or header file, or 'None'
-id_col_name = 'id_1' #column in csv file to look for image IDs
+ra_dec = 'csv' # 'csv', 'header', or None. Where to find ra and dec values if desired for naming. Otherwise will have 'N/A' in RA and DEC columns of results
+ra_dec_loc = '.csv' #path to csv file or header file, or None
+id_col_name = 'id_1' #column name in csv file to look for image IDs
 
 printMemory('Beginning')
 
